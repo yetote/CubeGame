c29b90d8d5194c43b0524e18d18d30479fa34f67
yetote <503779938@qq.com>
2018-01-16 16:10:10 +0800

    Initial commit

---
+++ app/src/main/java/com/demo/yetote/cubegame/fragment/CommunityFragment.java
@@ -0,0 +1,26 @@
+package com.demo.yetote.cubegame.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.demo.yetote.cubegame.R;
+
+/**
+ * com.demo.yetote.cubegame.fragment
+ * 社区
+ * @author Swg
+ * @date 2018/1/16 14:28
+ */
+public class CommunityFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_community, container);
+        return v;
+    }
+}
---
+++ app/src/main/java/com/demo/yetote/cubegame/fragment/NewsFragment.java
@@ -0,0 +1,27 @@
+package com.demo.yetote.cubegame.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.demo.yetote.cubegame.R;
+
+/**
+ * com.demo.yetote.cubegame.fragment
+ * 咨讯
+ *
+ * @author Swg
+ * @date 2018/1/16 14:29
+ */
+public class NewsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_news, container);
+        return v;
+    }
+}
