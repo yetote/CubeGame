bb64dde0e6c5d0e1d6ae900f9780c437c2d3fa1e
yetote <503779938@qq.com>
2018-01-19 16:13:01 +0800

    第四天，没干啥

---
+++ app/src/main/java/com/demo/yetote/cubegame/utils/HomePagerGrouping.java
@@ -0,0 +1,20 @@
+package com.demo.yetote.cubegame.utils;
+
+import android.graphics.Rect;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * com.demo.yetote.cubegame.utils
+ * 添加分组信息
+ *
+ * @author Swg
+ * @date 2018/1/19 13:32
+ */
+public class HomePagerGrouping extends RecyclerView.ItemDecoration {
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        super.getItemOffsets(outRect, view, parent, state);
+
+    }
+}
