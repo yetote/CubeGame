48b9c3a1a2d9f44c66c5fc0fb4921639ca4f07ea
yetote <503779938@qq.com>
2018-03-06 17:36:09 +0800

    第二十三天：倒计时按钮完善，增加部分xml布局

--- app/src/main/java/com/demo/yetote/cubegame/utils/Count.java
+++ app/src/main/java/com/demo/yetote/cubegame/utils/Count.java
@@ -11,6 +11,7 @@ import android.util.Log;
  */
 public class Count {
     private static final String TAG = "Count";
+
     /**
      * 倒计时
      *
@@ -18,11 +19,10 @@ public class Count {
      * @return 字符类型的倒计时
      */
     public static String countTime(long date) {
-        int hour = (int) (date / 60 / 60 );
-        int min = (int) (date / 60 );
-        int sec = (int) (date % (60*60) );
-        Log.e(TAG, "countTime: "+date );
+        int hour = (int) (date / 60 / 60);
+        int min = (int) (date / 60);
+        int sec = (int) (date % 60);
+        Log.e(TAG, "countTime: " + date);
         return hour + ":" + min + ":" + sec;
     }
-
 }
