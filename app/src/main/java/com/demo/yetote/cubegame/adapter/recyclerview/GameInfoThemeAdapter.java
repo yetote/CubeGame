b4cc76df6cb923b20d71079c80c74c89fe645636
yetote <503779938@qq.com>
2018-03-22 22:29:17 +0800

    第三十天追加：上传部分被忽略的adapter，完成度55%:bug:

---
+++ app/src/main/java/com/demo/yetote/cubegame/adapter/recyclerview/GameInfoThemeAdapter.java
@@ -0,0 +1,88 @@
+package com.demo.yetote.cubegame.adapter.recyclerview;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.demo.yetote.cubegame.R;
+import com.demo.yetote.cubegame.model.GameInfoThemeModel;
+import com.demo.yetote.cubegame.utils.OnClick;
+
+import java.util.ArrayList;
+
+/**
+ * com.demo.yetote.cubegame.adapter
+ *
+ * @author Swg
+ * @date 2018/2/8 14:05
+ */
+public class GameInfoThemeAdapter extends RecyclerView.Adapter {
+    private Context context;
+    private ArrayList<GameInfoThemeModel> list;
+    private OnClick listener;
+
+    public OnClick getListener() {
+        return listener;
+    }
+
+    public void setListener(OnClick listener) {
+        this.listener = listener;
+    }
+    public GameInfoThemeAdapter(Context context, ArrayList<GameInfoThemeModel> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    private class MyViewHolder extends RecyclerView.ViewHolder {
+        private TextView title, content, time, discussNum;
+
+        private TextView getTitle() {
+            return title;
+        }
+
+        private TextView getContent() {
+            return content;
+        }
+
+        private TextView getTime() {
+            return time;
+        }
+
+        private TextView getDiscussNum() {
+            return discussNum;
+        }
+
+        private MyViewHolder(View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.item_game_info_theme_title);
+            content = itemView.findViewById(R.id.item_game_info_theme_content);
+            time = itemView.findViewById(R.id.item_game_info_theme_time);
+            discussNum = itemView.findViewById(R.id.item_game_info_theme_discussNum);
+        }
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.item_game_info_theme, parent, false);
+        v.setOnClickListener(v1 -> listener.onClickListener(v, (String) v.getTag()));
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        MyViewHolder vh = (MyViewHolder) holder;
+        vh.getContent().setText(list.get(position).getContent());
+        vh.getTitle().setText(list.get(position).getTitle());
+        vh.getTime().setText(list.get(position).getTime());
+        vh.getDiscussNum().setText(list.get(position).getDiscussNum());
+        vh.itemView.setTag(list.get(position).getId());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+}
